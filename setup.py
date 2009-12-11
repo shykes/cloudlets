b705eabbaa5242d1acbf67009a841c9c279bc92f
Solomon Hykes <solomon.hykes@dotcloud.com>
2009-12-11 12:46:26 +0000

    Use distribute (http://python-distribute.org) to handle dependencies automatically. For now this only works with deps available on Pypi. Other deps are listed in requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
 
+from distribute_setup import use_setuptools
+use_setuptools()
+
 setup(name='clapp',
       version='0.0.1',
       author='Solomon Hykes',
+	
+      install_requires=[
+	'simplejson >= 2.0.9'
+	],
       package_dir = {
           'clapp' : '.',
         },
