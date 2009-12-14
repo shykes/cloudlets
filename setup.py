2578be48792949413e8b8b9a9aa429e6b48b45fd
Solomon Hykes <solomon.hykes@dotcloud.com>
2009-12-14 15:28:52 +0100

    Better build system

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
 
-from distribute_setup import use_setuptools
-use_setuptools()
+from setuptools import setup
 
 setup(name='clapp',
       version='0.0.1',
       author='Solomon Hykes',
-      install_requires=['simplejson >= 2.0.9'],
+      install_requires=['distribute', 'simplejson >= 2.0.9', 'jsonschema', 'ejs >= 0.0.1'],
       package_dir = {'clapp' : '.'},
       packages=['clapp'],
-      scripts=['clapp.py']
+      scripts=['clapp.py'],
+      dependency_links=['http://dotcloud.org.s3.amazonaws.com/ejs-0.0.1.tar.gz', 'http://jsonschema.googlecode.com/files/jsonschema-0.2a.tar.gz']
 )
