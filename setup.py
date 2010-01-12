5b8ff8ffce7308b24e81bbec2d0f7210d6fe3072
Solomon Hykes <solomon.hykes@dotcloud.com>
2010-01-12 14:06:57 +0000

    New dependencies: mercurial and metashelf

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(name='cloudlets',
       version='0.0.1',
       author='Solomon Hykes',
-      install_requires=['simplejson >= 2.0.9', 'jsonschema', 'ejs >= 0.0.1', 'argparse'],
+      install_requires=['simplejson >= 2.0.9', 'jsonschema', 'ejs >= 0.0.1', 'argparse', 'metashelf == 0.0.1', 'mercurial'],
       package_dir = {'cloudlets' : '.'},
       packages=['cloudlets'],
       scripts=['bin/cloudlets'],
-      dependency_links=['http://dotcloud.org.s3.amazonaws.com/ejs-0.0.1.tar.gz', 'http://jsonschema.googlecode.com/files/jsonschema-0.2a.tar.gz']
+      dependency_links=['http://dotcloud.org.s3.amazonaws.com/metashelf-0.0.1.tar.gz', 'http://dotcloud.org.s3.amazonaws.com/ejs-0.0.1.tar.gz', 'http://jsonschema.googlecode.com/files/jsonschema-0.2a.tar.gz']
 )
