f2731b881ec488e2770a1989a8b13d844e72af14
Solomon Hykes <solomon.hykes@dotcloud.com>
2010-02-14 16:53:06 +0000

    Version bump: 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='cloudlets',
-      version='0.0.2',
+      version='0.0.3',
       author='Solomon Hykes',
       install_requires=['simplejson >= 2.0.9', 'jsonschema', 'ejs >= 0.0.1', 'argparse', 'metashelf == 0.0.1', 'mercurial', 'vm2vm'],
       package_dir = {'cloudlets' : '.'},
